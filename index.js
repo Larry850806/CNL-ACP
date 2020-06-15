7978b491ba2700f2762b1633b3b0224c62c6429d
Larry850806 <pudding850806@gmail.com>
2020-06-15 21:36:06 +0800

    Refactor index.js selector

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+const getCardSel = (cardIdx) => `.cards > .card:nth(${cardIdx})`
+const getCardNameSel = (cardIdx) => `.card .header:nth(${cardIdx})`
+const getInputSel = (inputIdx) => `input:nth(${inputIdx})`
+
 let selectedUser = null
 
 function initInputStatus() {
   alert('正在抓取 server 蒐集到的時間')
   let inputValues = [true, false, true, true] // get from api
   for (i in inputValues) {
-    let selector = `input:nth(${i})`
+    let selector = getInputSel(i)
     $(selector).prop('checked', inputValues[i])
   }
 }
@@ -12,7 +16,7 @@ function initInputStatus() {
 function handleUserSelect(idx) {
   selectedUser = idx
   for (let i = 0; i < 4; i++) {
-    let selector = `.cards > .card:nth(${i})`
+    let selector = getCardSel(i)
     if (i == idx) {
       $(selector).addClass('active')
     } else {
@@ -23,19 +27,19 @@ function handleUserSelect(idx) {
 }
 
 for (let i = 0; i < 4; i++) {
-  let selector = `.cards > .card:nth(${i})`
+  let selector = getCardSel(i)
   $(selector).click(() => handleUserSelect(i))
 }
 
 $('#submit').click(() => {
-  let name = $(`.card .header:nth(${selectedUser})`).text()
+  let name = $(getCardNameSel(selectedUser)).text()
   if (name === '') {
     alert('要先選你是誰才能送出哦')
     return
   }
 
   let inputValues = [0, 1, 2, 3].map((i) => {
-    let selector = `input:nth(${i})`
+    let selector = getInputSel(i)
     return $(selector).is(':checked')
   })
   alert(`你是${name}\n時段：${inputValues}`)
