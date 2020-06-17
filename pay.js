97670a0ae77b3fccbc018b2f59a20ed641850144
Larry850806 <pudding850806@gmail.com>
2020-06-17 09:21:47 +0800

    Finish pay

--- pay.js
+++ pay.js
@@ -5,6 +5,7 @@ const getDrop1ItemSel = (idx) => `#payer-dropdown .menu > .item:nth(${idx})`
 const getDrop2ItemSel = (idx) => `#payee-dropdown .menu > .item:nth(${idx})`
 
 $('input[type=hidden]').val('')
+$('input[name=amount]').val('')
 
 // init username and balance
 axios('http://140.112.225.211:8888/usage').then(({ data }) => {
@@ -39,16 +40,35 @@ function handleSplitClick() {
 }
 
 function handlePayClick() {
-  console.log('pay click')
+  let payer = $('#payer-dropdown .text').text()
+  let payee = $('#payee-dropdown .text').text()
+  let amount = +$('input[name=amount]').val()
+
+  console.log({ payer, payee, amount })
+  if (payer == '轉出帳號') {
+    alert('轉出帳號要填')
+    return
+  }
+  if (payee == '轉入帳號') {
+    alert('轉出帳號要填')
+    return
+  }
+  if (amount == 0) {
+    alert('金額要填')
+    return
+  }
+  if (payee == payer) {
+    alert('不可以轉給自己')
+    return
+  }
 
   // payer: 付款者的名字，只能是一般使用者
   // payee: 收款者的名字，當為’admin’時代表payer付錢給公帳，即是買冷氣卡的意思
   // amount: 付款的數目，整數。當payee = 'admin’時請填400
   let form = new FormData()
-
-  form.set('payer', '乖乖牌')
-  form.set('payee', '空氣人')
-  form.set('amount', 100)
+  form.set('payer', payer)
+  form.set('payee', payee)
+  form.set('amount', amount)
 
   axios({
     method: 'post',
@@ -57,9 +77,9 @@ function handlePayClick() {
     headers: { 'Content-Type': 'multipart/form-data' },
   })
     .then((data) => {
-      alert('送出成功')
+      alert('轉帳成功')
       console.log(data)
-      // location.reload()
+      location.reload()
     })
     .catch((err) => {
       console.error(err)
@@ -68,5 +88,11 @@ function handlePayClick() {
 
 $('button#split').click(handleSplitClick)
 $('button#pay').click(handlePayClick)
-
 $('.dropdown').dropdown()
+$('form.pay').keypress((e) => {
+  var keycode = event.keyCode ? event.keyCode : event.which
+  if (keycode == '13') {
+    // do nothing on Enter pressed
+    e.preventDefault()
+  }
+})
