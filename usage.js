99f2a21fa8e93f7eea31b9185a8869d5bba9dcc1
Larry850806 <pudding850806@gmail.com>
2020-06-17 08:16:54 +0800

    Add last split time in usage page

--- usage.js
+++ usage.js
@@ -1,15 +1,18 @@
 const getNameSel = (idx) => `div.name:nth(${idx})`
 const getRatioSel = (idx) => `div.progress div.progress:nth(${idx})`
 const getBarSel = (idx) => `div.bar:nth(${idx})`
+const getSplitTimeSel = () => `#last-split-time > span`
 
 // init username
 axios('http://140.112.225.211:8888/usage').then(({ data }) => {
-  data = data[1]
   console.log(data)
+  let splitTime = data[2]
 
+  data = data[1]
   let usernames = Object.keys(data)
   console.log(usernames)
 
+  $(getSplitTimeSel()).text(splitTime)
   usernames.map((name, idx) => {
     let ratio = Math.round(data[name] * 100) + '%'
     $(getNameSel(idx)).text(name)
